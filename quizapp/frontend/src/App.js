6ba030c15c64b8c3d4b0f7515f4fd0a3068ffccb
JFKerrigan <miss.supernothing@gmail.com>
2022-04-05 11:10:50 +0100

    link functionality in nav bar

--- quizapp/frontend/src/App.js
+++ quizapp/frontend/src/App.js
@@ -1,11 +1,20 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+import Register from './components/Register';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Test</h1>
+        <BrowserRouter>
+            <Nav />
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/register' element={<Register />} />
+            </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ quizapp/frontend/src/components/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            TEST REGISTER
+        </div>
+    )
+}
+
+export default Register
